bd9c24445a8daa7ba2c6a356e4387b056dc78689
Bartosz Zanijat <s34083@pjwstk.edu.pl>
2026-03-12 13:39:45 +0100

    Avrage

--- Program.cs
+++ Program.cs
@@ -6,4 +6,18 @@ class Program
     {
         Console.WriteLine("Stats. Pending...");
     }
+    static double CalculateAverage(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return 0;
+        }
+
+        int suma = 0;
+        foreach (int liczba in values)
+        {
+            suma += liczba;
+        }
+        return (double)suma / values.Length;
+    }
 }
